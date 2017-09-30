821f1a80d351b6832f3f435c594bfaae52f9a39a
Shydos <mathieu.roussilhe@gmail.com>
2017-09-30 14:59:41 +0200

    storage

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  score: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private storage: Storage) {
+    storage.set('score', 12);
+    storage.get('score').then((val) => {
+      this.score = val;
+    });
   }
 
 }
