276b8d6d2f19d574762169a48fb80fd33b4afdde
Unknown <mathieu.roussilhe@gmail.com>
2017-11-03 18:45:26 +0100

    Stockage ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +7,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  smileys: {smiley: string, latitute: string, longitude: string}[];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private storage: Storage) {
+    this.storage.get('smileys').then((val) => {
+      if (val === null) {
+        this.smileys = [];
+      } else {
+        this.smileys = val;
+      }
+    });
   }
 
+  smileyClick(icon:string) {
+    this.smileys.push({smiley: icon, latitute: "234234", longitude: "12123123"});
+    this.storage.set('smileys', this.smileys);
+    console.log(this.smileys);
+  }
 }
