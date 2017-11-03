0ba903385cd148ee25fb54c01bbe9f2511f9c998
Luidgy <fluidgy@gmail.com>
2017-11-03 14:29:50 +0100

    geoloc

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { SocialSharing } from '@ionic-native/social-sharing';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-about',
@@ -9,8 +10,16 @@ import { SocialSharing } from '@ionic-native/social-sharing';
 })
 export class AboutPage {
   scores: string[];
+  latitude: number;
+  longitude: number;
 
-  constructor(public navCtrl: NavController, private storage: Storage, private socialSharing: SocialSharing) {
+  constructor(public navCtrl: NavController, private storage: Storage, private socialSharing: SocialSharing, private geolocation: Geolocation) {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
     storage.set('scores', [121, 123213, 4344]);
 
     storage.get('scores').then((val) => {
