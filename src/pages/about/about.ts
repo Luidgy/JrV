13ad96ca6402bacc4e760521abcf63a039f76aed
Luidgy <fluidgy@gmail.com>
2017-11-04 15:50:22 +0100

    ajout

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { Geolocation } from '@ionic-native/geolocation';
 import { SocialSharing } from '@ionic-native/social-sharing';
-import { GoogleMaps, GoogleMap} from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, CameraPosition, LatLng, GoogleMapsEvent, Marker, MarkerOptions } from '@ionic-native/google-maps';
 
 declare var google;
 
@@ -14,10 +14,14 @@ declare var google;
 export class AboutPage {
   @ViewChild('map') mapElement: ElementRef;
   map: GoogleMap;
-
+  latitude: number;
+  longitude: number;
   scores: string[];
 
-  constructor(public navCtrl: NavController, private storage: Storage, private socialSharing: SocialSharing, private _googleMaps: GoogleMaps) {
+
+
+
+  constructor(public navCtrl: NavController, private storage: Storage, private socialSharing: SocialSharing, private _googleMaps: GoogleMaps, private _geoloc: Geolocation) {
 
     storage.set('scores', [121, 123213, 4344]);
 
@@ -34,11 +38,36 @@ export class AboutPage {
     });
   }
   ngAfterViewInit(){
+    let loc: LatLng;
     this.initMap();
+    this.getLocation().then(res => {
+    loc = new LatLng(res.coords.latitude, res.coords.longitude);
+    this.createMarker(loc, "Me").then((marker: Marker) => {
+      marker.showInfoWindow();
+    }).catch(err => {
+      console.log(err);
+    });
+  }).catch(err => {
+    console.log(err);
+  });
+
   }
 
   initMap(){
     let element = this.mapElement.nativeElement;
     this.map = this._googleMaps.create(element)
   }
+
+  getLocation(){
+    return this._geoloc.getCurrentPosition();
+  }
+
+  createMarker(loc: LatLng, title: string){
+    let markerOptions: MarkerOptions = {
+      position: loc,
+      title: title
+    };
+    return this.map.addMarker(markerOptions);
+  }
+
 }
